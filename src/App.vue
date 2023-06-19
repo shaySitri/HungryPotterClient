5b2a55d37e54c626c4b9bdfa520c2e72c26257b5
ShaySitri <sitri@post.bgu.ac.il>
2023-06-20 01:20:55 +0300

    navigator

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,76 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'main' }">Vue Recipes</router-link>|
-      <router-link :to="{ name: 'search' }">Search</router-link>|
-      {{ !$root.store.username }}
-      <span v-if="!$root.store.username">
-        Guest:
-        <router-link :to="{ name: 'register' }">Register</router-link>|
-        <router-link :to="{ name: 'login' }">Login</router-link>|
-      </span>
-      <span v-else>
-        {{ $root.store.username }}: <button @click="Logout">Logout</button>|
-      </span>
-    </div>
-    <router-view />
+  <div id="app" @logged="location.reload()">
+    
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info" >
+      <b-navbar-brand href="#">Hungrry Potter</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item><router-link :to="{ name: 'main' }">Food Spells</router-link></b-nav-item>
+        <b-nav-item><router-link :to="{ name: 'search' }">Search</router-link></b-nav-item>
+        <b-nav-item><router-link :to="{ name: 'about' }">About</router-link></b-nav-item>
+
+        <b-nav-item-dropdown text="Personal Zone" right v-show="$root.store.shared_data.username">
+          <b-dropdown-item><router-link :to="{ name: 'about' }">My Food Spells</router-link></b-dropdown-item>
+          <b-dropdown-item><router-link :to="{ name: 'about' }">Family Food Spells</router-link></b-dropdown-item>
+          <b-dropdown-item><router-link :to="{ name: 'about' }">Favorite Spells</router-link></b-dropdown-item>
+        </b-nav-item-dropdown>
+
+      </b-navbar-nav>
+
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-form right v-show="!$root.store.shared_data.username">
+        Hey Muggle! 
+        <login-nav-bar></login-nav-bar>
+        <b-nav-item><router-link :to="{ name: 'register' }">Register</router-link></b-nav-item>
+        </b-nav-form>
+ 
+
+        <b-nav-item-dropdown right v-show="$root.store.shared_data.username">
+        <!-- Using 'button-content' slot -->
+        <template #button-content>
+        <em>Hey {{ $root.store.shared_data.firstname}}</em>
+        </template>
+        <b-dropdown-item href="#">Profile</b-dropdown-item>
+        <b-dropdown-item @click="Logout">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+        
+
+      </b-navbar-nav>
+</b-collapse>
+</b-navbar>
+</div>
+<router-view />
   </div>
 </template>
 
 <script>
+import LoginNavBar from './components/LoginNavBar.vue';
+
 export default {
   name: "App",
+  components:
+  {
+    LoginNavBar
+  },
   methods: {
     Logout() {
-      this.$root.store.logout();
+      this.$root.store.shared_data.logout();
       this.$root.toast("Logout", "User logged out successfully", "success");
 
       this.$router.push("/").catch(() => {
         this.$forceUpdate();
       });
-    }
+      location.reload();
+
+    },
+
   }
 };
 </script>
@@ -42,6 +84,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   min-height: 100vh;
+  font-size: larger;
 }
 
 #nav {
